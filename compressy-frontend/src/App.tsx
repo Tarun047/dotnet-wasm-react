7f9f2b8edefcb9534462b61fd8734715cf89aab8
Tarun <gtarun047@gmail.com>
2023-09-04 13:57:20 +0530

    Findings without web workers

--- compressy-frontend/src/App.tsx
+++ compressy-frontend/src/App.tsx
@@ -4,15 +4,23 @@ import {WorkerCompressionResult, WorkerMessage, WorkerMessageType} from "./worke
 function App() {
     const [nonNativeWorker, setNonNativeWorker] = React.useState<Worker>()
     const [nonNativeResult, setNonNativeResult] = React.useState<string>();
-    const [nativeWorker, setNativeWorker] = React.useState<Worker>();
+    const [compressor, setCompressor] = React.useState<{
+        initializeRuntimeExports: () => Promise<void>,
+        nativeCompress: (data: Uint8Array) => Promise<Uint8Array>
+    }>();
     const [nativeResult, setNativeResult] = React.useState<string>();
+
+    async function initializeCompressor() {
+        const compressorInstance = await import(/* webpackIgnore: true */ './vendor/main.js');
+        await compressorInstance.initializeRuntimeExports();
+        setCompressor(compressorInstance);
+    }
+
     useEffect(() => {
         const nonNativeWorker = new Worker(new URL('workers/non-native-worker', import.meta.url), {type: "module"});
         nonNativeWorker.onmessage = handleNonNativeWorkerMessage
         setNonNativeWorker(nonNativeWorker);
-        const nativeWorker = new Worker(new URL('workers/native-worker', import.meta.url), {type: "module"});
-        nativeWorker.onmessage = handleNativeWorkerMessage
-        setNativeWorker(nativeWorker)
+        initializeCompressor();
     }, [])
 
     function handleNonNativeWorkerMessage(event: MessageEvent<WorkerMessage<unknown>>) {
@@ -22,16 +30,6 @@ function App() {
         } else if (workerEvent.type == WorkerMessageType.FinishedCompression) {
             const result = workerEvent.data as WorkerCompressionResult;
             setNonNativeResult(`Non native worker finished Compressing ${result.data.length} bytes in ${result.time} ms`)
-        }
-    }
-
-    function handleNativeWorkerMessage(event: MessageEvent<WorkerMessage<unknown>>) {
-        const workerEvent = event.data;
-        if (workerEvent.type == WorkerMessageType.StartedCompression) {
-            setNativeResult("Started Compressing")
-        } else if (workerEvent.type == WorkerMessageType.FinishedCompression) {
-            const result = workerEvent.data as WorkerCompressionResult;
-            setNativeResult(`Native Worker finished Compressing to ${result.data.length} bytes in ${result.time} ms`)
         }
     }
 
@@ -49,7 +47,14 @@ function App() {
         if ((!files || files.length < 0)) {
             setNativeResult("Hmm you didn't chose a file")
         } else {
-            nativeWorker?.postMessage(files[0])
+            const data = new Uint8Array(await files[0].arrayBuffer());
+            setNativeResult("Started Compressing")
+            if (compressor) {
+                const startTime = performance.now();
+                const result = await compressor.nativeCompress(data)
+                const endTime = performance.now();
+                setNativeResult(`Native implementation finished Compressing to ${result.byteLength} bytes in ${endTime - startTime} ms`)
+            }
         }
     }
 
